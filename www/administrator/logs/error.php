5700fc404b15f9a0dcd3a5392cdfc3b1bbee3afa
nastasios <nastasiosgiannoulis@Nastasios-iMac.local>
2018-10-18 00:09:44 +0200

    first commit

---
+++ www/administrator/logs/error.php
@@ -0,0 +1,7 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2018-10-17 22:02:33 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2018-10-17T22:02:33+00:00	INFO 172.17.0.1	joomlafailure	Username and password do not match or you do not have an account yet.
